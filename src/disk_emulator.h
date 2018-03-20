54146f1e2891db7bc0325d3d2661e9b21a93527a
github-travis-travis <37538918+github-travis-travis@users.noreply.github.com>
2018-03-20 13:20:47 +0530

    Update disk_emulator.h

--- src/disk_emulator.h
+++ src/disk_emulator.h
@@ -6,7 +6,7 @@
 int init_disk_emulator(void);
 void free_disk_emulator(void);
 
-//int read_block(big_int block_id, void* target);
-//int write_block(big_int block_id, void* buffer, size_t buffer_size);
+int read_block(big_int block_id, void* target);
+int write_block(big_int block_id, void* buffer, size_t buffer_size);
 
 #endif
